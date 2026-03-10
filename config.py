418ccfaa28c87128c5aeb8e92c3bcb7a9438415f
AppMotoristaPro <appmotoristapro@gmail.com>
2026-03-10 17:33:18 -0300

    a

--- config.py
+++ config.py
@@ -1,16 +1,22 @@
 import os
 from dotenv import load_dotenv
 
+# Carrega as variáveis do arquivo .env (útil para quando você for testar no Termux)
 load_dotenv()
 
 class Config:
+    # 1. Configurações Básicas
     SECRET_KEY = os.environ.get("SECRET_KEY", "chave-super-secreta-gestao-ti")
+    
+    # 2. Banco de Dados
     DATABASE_URL = os.environ.get("DATABASE_URL")
     
-    # Chaves para a Notificação Push (Web Push API)
-    VAPID_PUBLIC_KEY = os.environ.get("VAPID_PUBLIC_KEY", "BBN1tLnB8VQRegpxtWOXIcWzCKGDdI2Zvxu93Bdt64Tur25vnp_JnQw3vhN6pmyyxPMS8DBhrMuB4xajBF6b4Ps")
-    VAPID_PRIVATE_KEY = os.environ.get("VAPID_PRIVATE_KEY", "blGtF9rkYMmE_8dYLkmm1w8CeG0yVJRhmCHrA2J6q7Q")
+    # 3. Chaves para a Notificação Push (Web Push API)
+    # Lendo apenas das variáveis de ambiente para proteção contra vazamentos
+    VAPID_PUBLIC_KEY = os.environ.get("VAPID_PUBLIC_KEY")
+    VAPID_PRIVATE_KEY = os.environ.get("VAPID_PRIVATE_KEY")
     VAPID_CLAIM_EMAIL = os.environ.get("VAPID_CLAIM_EMAIL", "mailto:igor@despesasti.com")
     
+    # 4. Segurança de Upload
     MAX_CONTENT_LENGTH = 10 * 1024 * 1024
 
